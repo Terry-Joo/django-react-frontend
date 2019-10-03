595a15f578b4b5850f8c967f40116d87a0f888b9
yungik <yungik@glocalizeinc.com>
2019-10-03 23:57:45 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {Posts} from "./pages";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function Cover() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
       </header>
     </div>
-  );
+  )
 }
+
+const App = () => (
+  <div>
+    <Route exact path="/" component={Cover}/>
+    <Route path="/posts" component={Posts}/>
+  </div>
+);
+
 
 export default App;
